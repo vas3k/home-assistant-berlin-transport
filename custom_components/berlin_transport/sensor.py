0cd432f8fbb89f8fe67455c0aa290bede29c17e9
Volodymyr Denisiuk <denisyuk.v1@gmail.com>
2022-12-12 09:25:20 +0100

    Fix: Linter error fix, line 157 too long

--- custom_components/berlin_transport/sensor.py
+++ custom_components/berlin_transport/sensor.py
@@ -153,7 +153,8 @@ class TransportSensor(SensorEntity):
             return []
 
         # convert api data into objects
-        return sorted([Departure.from_dict(departure) for departure in departures], key=lambda d: d.time)
+        unsorted = [Departure.from_dict(departure) for departure in departures]
+        return sorted(unsorted, key=lambda d: d.time)
 
     def next_departure(self):
         if self.departures and isinstance(self.departures, list):
