64d334b0aadc2a4fbf0dddf483fef7a3d1402361
Manuel Rüger <manuel@rueg.eu>
2024-01-09 19:15:23 +0100

    Remove unneeded functions

--- custom_components/berlin_transport/__init__.py
+++ custom_components/berlin_transport/__init__.py
@@ -24,11 +24,6 @@ async def config_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) 
     await hass.config_entries.async_reload(entry.entry_id)
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-
 def setup(
     hass: HomeAssistant, config: ConfigType  # pylint: disable=unused-argument
 ) -> bool:
