f05df06fce442c420a1fc681c0e3e65f0e0ce4f6
Manuel Rüger <manuel@rueg.eu>
2024-01-10 15:03:07 +0100

    Add delay to departure
    This is the delay in seconds

--- custom_components/berlin_transport/departure.py
+++ custom_components/berlin_transport/departure.py
@@ -20,6 +20,7 @@ class Departure:
     fallback_color: str | None = None
     location: tuple[float, float] | None = None
     cancelled: bool = False
+    delay: int | None = None
 
     @classmethod
     def from_dict(cls, source):
@@ -43,6 +44,7 @@ class Departure:
                 source.get("currentTripPosition", {}).get("longitude") or 0.0,
             ],
             cancelled=source.get("cancelled", False),
+            delay=source.get("delay", None),
         )
 
     def to_dict(self, show_api_line_colors: bool):
@@ -56,4 +58,5 @@ class Departure:
             "direction": self.direction,
             "color": color,
             "cancelled": self.cancelled,
+            "delay": self.delay,
         }
