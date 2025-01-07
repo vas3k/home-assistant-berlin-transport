2e08ee926ab1326fd8ee204e970e490b1b2bfe6e
Manuel Rüger <manuel@rueg.eu>
2025-01-07 18:42:07 +0100

    feat: use session for connection pooling (#42)
    This avoids creating a new connection for each request

--- custom_components/berlin_transport/sensor.py
+++ custom_components/berlin_transport/sensor.py
@@ -104,6 +104,7 @@ class TransportSensor(SensorEntity):
         self.walking_time: int = config.get(CONF_DEPARTURES_WALKING_TIME) or 1
         # we add +1 minute anyway to delete the "just gone" transport
         self.show_api_line_colors: bool = config.get(CONF_SHOW_API_LINE_COLORS) or False
+        self.session: requests.Session = requests.Session()
 
     @property
     def name(self) -> str:
@@ -141,7 +142,7 @@ class TransportSensor(SensorEntity):
 
     def fetch_directional_departure(self, direction: str | None) -> list[Departure]:
         try:
-            response = requests.get(
+            response = self.session.get(
                 url=f"{API_ENDPOINT}/stops/{self.stop_id}/departures",
                 params={
                     "when": (
