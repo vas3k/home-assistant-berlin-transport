0d1d5e59bd77507ebc1ac6fe4b1948cd22cd5ecf
Cornelius Hoffmann <coding@volucra.de>
2026-09-08 23:29:24 +0200

    chore: sort imports

--- custom_components/berlin_transport/__init__.py
+++ custom_components/berlin_transport/__init__.py
@@ -10,13 +10,13 @@ from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.typing import ConfigType
 
 from .const import (
-    DOMAIN,  # noqa
     CONF_API_ENDPOINT,
     CONF_API_MAX_RESULTS,
     CONF_FALLBACK_TIME,
     CONF_UNIQUE_ID,
     CONFIG_ENTRY_VERSION,
     DEFAULT_API_ENDPOINT,
+    DOMAIN,  # noqa
     SUBENTRY_TYPE_STOP,
 )
 from .helpers import normalized_list_options
